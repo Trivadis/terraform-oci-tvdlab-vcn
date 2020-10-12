52642b39af395441b700dfd7d72183eccbdcb5f8
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-10-12 10:13:06 +0200

    add outupt

--- outputs.tf
+++ outputs.tf
@@ -19,33 +19,48 @@
 
 # get lab compartment resource information
 output "vcn_id" {
-  description = "ocid of created VCN. "
-  value       = "${oci_core_vcn.*.id}"
+  description = "id of vcns which are created"
+  value       = oci_core_vcn.vcn.*.id
 }
+
+# output "nat_gateway_id" {
+#   description = "id of nat gateway if it is created"
+#   value       = join(",", oci_core_nat_gateway.nat_gateway.*.id)
+# }
+
+output "internet_gateway_id" {
+  description = "id of internet gateway if it is created"
+  value = join(",", oci_core_internet_gateway.igw.*.id)
+}
+
+# output "ig_route_id" {
+#   description = "id of internet gateway route table"
+#   value       = join(",", oci_core_route_table.ig.*.id)
+# }
+
+# output "nat_route_id" {
+#   description = "id of VCN NAT gateway route table"
+#   value       = join(",", oci_core_route_table.nat.*.id)
+# }
 
 output "default_security_list_id" {
   description = "ocid of default security list. "
-  value       = "${oci_core_vcn.*.default_security_list_id}"
+  value       = oci_core_vcn.vcn.*.default_security_list_id
 }
 
 output "default_dhcp_options_id" {
   description = "ocid of default DHCP options. "
-  value       = "${oci_core_vcn.*.default_dhcp_options_id}"
+  value       = oci_core_vcn.vcn.*.default_dhcp_options_id
 }
 
 output "default_route_table_id" {
   description = "ocid of default route table. "
-  value       = "${oci_core_vcn.*.default_route_table_id}"
+  value       = oci_core_vcn.vcn.*.default_route_table_id
 }
 
-output "internet_gateway_id" {
-  description = "ocid of internet gateway. "
-  value       = "${oci_core_internet_gateway.*.id}"
-}
-
-output "subnet_ids" {
-  description = "ocid of subnet ids. "
-  value       = "${oci_core_subnet.this.*.id}"
-}
+# output "subnet_ids" {
+#   description = "ocid of subnet ids. "
+#   value       = "${oci_core_subnet.vcn.*.id}"
+# }
 
 # --- EOF -------------------------------------------------------------------
